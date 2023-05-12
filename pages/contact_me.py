a18bad7fa51a83f2107f5b19e8cd5f8ad639ed34
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-05-12 16:43:17 +0900

    completed the send email feature

--- pages/contact_me.py
+++ pages/contact_me.py
@@ -1,11 +1,18 @@
-import requests
+# import requests
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Email Address")
     message = st.text_area("Message")
+    email = f"""\
+Subject: New email
+From: {user_email}
+Body: {message}
+        """
     button = st.form_submit_button()
     if button:
-        message = message + user_email
+        send_email(message)
+        st.info("Email sent succesfully")
--- send_email.py
+++ send_email.py
@@ -2,21 +2,19 @@ from dotenv import load_dotenv
 import os
 import smtplib, ssl
 
-load_dotenv()
+def send_email(message):
 
-username = os.environ.get("gmail_username")
-password = os.environ.get("gmail_password")
+    load_dotenv()
 
-host = "smtp.gmail.com"
-port = 465
+    username = os.environ.get("gmail_username")
+    password = os.environ.get("gmail_password")
 
-receiver = "manudelaforest@gmail.com"
-context = ssl.create_default_context()
+    host = "smtp.gmail.com"
+    port = 465
 
-message = """
-Test
-"""
+    receiver = "emmanuel.delaforestdivonne@gmail.com"
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
